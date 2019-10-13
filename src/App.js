c71d51e12313118cd3d16fb4feadb14fff9d9042
macbook <macbook@84.local>
2019-10-13 18:03:30 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload ilker Karapanca I love you.
+          Edit <code>src/App.js</code> and save to reload Iskender leoyu cok seviyor.
         </p>
         <a
           className="App-link"
